a6808de660f1e7d6b3453769a7ea59300fc641ea
Akim <gentilakili98@gmail.com>
2022-08-28 15:07:06 +0200

    Init context

---
+++ admin/src/contexts/DataItems.jsx
@@ -0,0 +1,28 @@
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+
+const items = {};
+
+items.links = [
+  {
+    name: "Dashboard",
+    icon: <MdOutlineDashboardCustomize />,
+  },
+  {
+    name: "Cours",
+    icon: <MdOutlineDashboardCustomize />,
+  },
+  {
+    name: "Participants",
+    icon: <MdOutlineDashboardCustomize />,
+  },
+  {
+    name: "Paiement",
+    icon: <MdOutlineDashboardCustomize />,
+  },
+  {
+    name: "Settings",
+    icon: <MdOutlineDashboardCustomize />,
+  },
+];
+
+export default items;
---
+++ admin/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export{default as Dashboard} from './Dashboard';
+export {default as Cours} from './Cours';
+export {default as Participants} from './Participants';
+export {default as Settings} from './Settings';
+export {default as Paiement} from './Paiement';
